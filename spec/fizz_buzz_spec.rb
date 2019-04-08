f3d31296d934bbc11dba34f63b1ec2165fa1fe1d
sbx_user1051 <sbx_user1051@169.254.84.193>
2019-04-08 10:36:16 +0000

    Mon Apr 08 10:36:16 UTC 2019

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,4 +1,13 @@
 require_relative '../lib/solutions/FIZ/fizz_buzz'
 
 describe FizzBuzz do
+
+before :each do
+  @fizzBuzz = FizzBuzz.new
+end
+
+it 'returns fizz if a number is divisible by 3' do
+  expect(@fizzBuzz.fizz_buzz(3)).to eq 'fizz'
+end
+
 end
