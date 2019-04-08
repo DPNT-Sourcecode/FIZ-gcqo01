83360edfe6a224d20034923ebbcee93633f56543
sbx_user1051 <sbx_user1051@169.254.84.193>
2019-04-08 10:42:16 +0000

    Mon Apr 08 10:42:16 UTC 2019

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -18,4 +18,12 @@ it 'returns fizz buzz if a number is divisible by 3 and 5' do
   expect(@fizzBuzz.fizz_buzz(15)).to eq 'fizz buzz'
 end
 
+it 'returns the provided number as a string if not divisible by 3/5/15' do
+  expect(@fizzBuzz.fizz_buzz(1)).to eq '1'
+end
+
+it 'raises an error if the provided number is outside the range 1-9999' do
+  expect{ @fizzBuzz.fizz_buzz(0) }.to raise_error 'Input is outside the permited range'
+end
+
 end
