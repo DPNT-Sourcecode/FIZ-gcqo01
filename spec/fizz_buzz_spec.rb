3fb9b35f8fe92947391babb3fd9dda8a67cbed98
sbx_user1051 <sbx_user1051@169.254.84.193>
2019-04-08 10:39:16 +0000

    Mon Apr 08 10:39:16 UTC 2019

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -10,4 +10,12 @@ it 'returns fizz if a number is divisible by 3' do
   expect(@fizzBuzz.fizz_buzz(3)).to eq 'fizz'
 end
 
+it 'returns buzz if a number is divisible by 5' do
+  expect(@fizzBuzz.fizz_buzz(5)).to eq 'buzz'
+end
+
+it 'returns fizz buzz if a number is divisible by 3 and 5' do
+  expect(@fizzBuzz.fizz_buzz(15)).to eq 'fizz buzz'
+end
+
 end
