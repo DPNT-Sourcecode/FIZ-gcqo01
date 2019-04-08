0efc9ccece19de46aed061110f4f7fbaccad4f16
sbx_user1051 <sbx_user1051@169.254.213.253>
2019-04-08 09:53:04 +0000

    Mon Apr 08 09:53:04 UTC 2019

--- spec/sum_spec.rb
+++ spec/sum_spec.rb
@@ -1,8 +1,16 @@
 require_relative '../lib/solutions/SUM/sum.rb'
 
 describe Sum do
-  it 'returns the sum of two parameters' do
+
+  before :each do
     sum = Sum.new
+  end
+
+  it 'returns the sum of two parameters' do
     expect(sum.sum(1,1)).to eq(2)
+  end
+
+  it 'raises an error if either parameter is negative' do
+    expect(sum.sum(-1,1)).to raise_error
   end
 end
