20cd6a55db8da26662be61c1165ed12bd1f58c9f
sbx_user1051 <sbx_user1051@169.254.213.253>
2019-04-08 09:56:04 +0000

    Mon Apr 08 09:56:04 UTC 2019

--- lib/solutions/SUM/sum.rb
+++ lib/solutions/SUM/sum.rb
@@ -2,6 +2,7 @@
 class Sum
 
   def sum(x, y)
+    raise 'Parameter is negative' if x < 0 || y < 0
     return x + y
   end
 
--- spec/sum_spec.rb
+++ spec/sum_spec.rb
@@ -2,15 +2,15 @@ require_relative '../lib/solutions/SUM/sum.rb'
 
 describe Sum do
 
-  before :each do
-    sum = Sum.new
+  before (:each) do
+    @sum = Sum.new
   end
 
   it 'returns the sum of two parameters' do
-    expect(sum.sum(1,1)).to eq(2)
+    expect(@sum.sum(1,1)).to eq(2)
   end
 
   it 'raises an error if either parameter is negative' do
-    expect(sum.sum(-1,1)).to raise_error
+    expect(@sum.sum(-1,1)).to raise_error
   end
 end
