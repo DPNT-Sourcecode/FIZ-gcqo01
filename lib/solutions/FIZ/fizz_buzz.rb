159f8ad830dc52f6b247a97a4894fb77add64221
sbx_user1051 <sbx_user1051@169.254.84.193>
2019-04-08 10:58:20 +0000

    Mon Apr 08 10:58:20 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -3,11 +3,27 @@ class FizzBuzz
 
   def fizz_buzz(number)
     raise 'Input is outside the permited range' if number < 1 || number > 9999
-    return 'fizz buzz' if (number % 15 == 0)
-    return 'fizz' if (number % 3 == 0) || number.to_s.split('').include?('3')
-    return 'buzz' if (number % 5 == 0) || number.to_s.split('').include?('5')
+    return 'fizz buzz' if fizz_buzz?(number)
+    return 'fizz' if fizz?(number)
+    return 'buzz' if buzz?(number)
     return number.to_s
   end
 
+  def fizz?(number)
+    return true if (number % 3 == 0) || number.to_s.split('').include?('3')
+    return false
+  end
+
+  def buzz?(number)
+    return true if (number % 5 == 0) || number.to_s.split('').include?('5')
+    return false
+  end
+
+  def fizz_buzz?(number)
+    return true if fizz?(number) && buzz?(number)
+    return false
+  end
+
 end
+
 
