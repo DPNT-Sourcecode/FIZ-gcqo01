d709092744ac0b52e2233cf0aa41f9e05dee35d7
sbx_user1051 <sbx_user1051@169.254.84.193>
2019-04-08 11:04:44 +0000

    Mon Apr 08 11:04:44 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -6,6 +6,7 @@ class FizzBuzz
     return 'fizz buzz' if fizz_buzz?(number)
     return 'fizz' if fizz?(number)
     return 'buzz' if buzz?(number)
+    return 'deluxe' if deluxe?(number)
     return number.to_s
   end
 
@@ -24,6 +25,11 @@ class FizzBuzz
     return false
   end
 
+  def deluxe?(number)
+    return true if number > 10 && number.to_s.split('').uniq.length == 1
+  end
+
 end
+
 
 
