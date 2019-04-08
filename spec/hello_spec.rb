013ef1cc018b46825aebc72a5f6bb33f0e547809
sbx_user1051 <sbx_user1051@169.254.156.1>
2019-04-08 10:17:50 +0000

    Mon Apr 08 10:17:50 UTC 2019

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -1,1 +1,12 @@
 require_relative '../lib/solutions/HLO/hello'
+
+describe Hello do
+  before :each do
+    @hello = Hello.new
+  end
+
+  it 'returns Hello World' do
+    expect(@hello.hello('World')).to eq 'Hello, World'
+  end
+
+end
